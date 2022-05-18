755ac924751d0f63daf01ad4f5260b01cadbbb7b
Stephen Wong <yummywongttv@gmail.com>
2022-05-18 15:31:05 -0400

    updated syntax

--- ProjectLucian/src/components/home section/welcome/Welcome.jsx
+++ ProjectLucian/src/components/home section/welcome/Welcome.jsx
@@ -1,50 +1,40 @@
 import './welcome.scss'
 
 import HomeSection from '../HomeSection'
+// import Button from '../../button/Button'
 
 
 import {
-    bg1
+    bg1,
 } from '../../../assets/images'
 
 const Welcome = props => {
-    return (
-        // <HomeSection
-        //     className={`welcome ${props.isActive ? 'active' : ''}`}
-        //     contentClassName="overlay welcome__content"
-        //     bgImage={bg1}
-        // >
-        //     <div className="welcome__info relative">
-        //         <div className="welcome__info__content">
-        //             <div className="title">
-        //                 <span>Welcome To</span>
-        //                 <h2 className='main-color'>Summoner's Rift</h2>
-        //             </div>
-        //             <div className='description m-t-4'>
 
-        //             </div>
-            <HomeSection
-                className={`welcome ${props.isActive ? 'active' : ''}`}
-                contentClassName="overlay welcome__content"
-                bgImage={bg1}
-            >
-                <div className="welcome__info relative">
-                    <div className="welcome__info__content">
-                        <div className="title">
-                            <span>Welcome To</span>
-                            <h2 className="main-color">Summoner's Rift</h2>
-                        </div>
-                        <div className="description m-t-4">
-                            Team up with friends and test your skills in 5v5 MOBA combat. All the high-skill competition you crave, designed especially for mobile and console with revamped controls and streamlined matches.Explore the living universe of Runeterra through lore, comics, games, and more. Then dive into the community of gamers, cosplayers, musicians, and content creators who are waiting for you to join them.
-                        </div>
-                        <div className="btns m-t-4">
-                            <Button className="btn-main">PLAY NOW</Button>
-                            <Button className="btn-second">GET STARTED</Button>
-                        </div>
+    return (
+        <HomeSection
+            className={`welcome ${props.isActive ? 'active' : ''}`}
+            contentClassName="overlay welcome__content"
+            bgImage={bg1}
+        >
+            <div className="welcome__info relative">
+                <div className="welcome__info__content">
+                    <div className="title">
+                        <span>Welcome To</span>
+                        <h2 className="main-color">Summoner's Rift</h2>
                     </div>
+                    <div className="description m-t-4">
+                        Team up with friends and test your skills in 5v5 MOBA combat. All the high-skill competition you crave, designed especially for mobile and console with revamped controls and streamlined matches.Explore the living universe of Runeterra through lore, comics, games, and more. Then dive into the community of gamers, cosplayers, musicians, and content creators who are waiting for you to join them.
+                    </div>
+                    {/* <div className="btns m-t-4">
+                        <Button className="btn-main">PLAY NOW</Button>
+                        <Button className="btn-second">GET STARTED</Button>
+                    </div> */}
                 </div>
-                <div className="welcome__img relative"></div>
-            </HomeSection>
+            </div>
+            <div className="welcome__img relative">
+                
+            </div>
+        </HomeSection>
     )
 }
 
